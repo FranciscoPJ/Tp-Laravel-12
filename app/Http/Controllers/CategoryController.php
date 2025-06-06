edb70ab68acd8131c33d55a1c552c316f1d414e2
Francisco Pandolfi Jimenez <francisco.pandolfi@est.fi.uncoma.edu.ar>
2025-06-06 03:07:43 -0300

    agregue la logica para que un user no acceda a editar blog, si el/la no es la autora y tire un error 403 si ademas quiera acceder de la url

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CategoryController extends Controller
 {
@@ -17,27 +18,29 @@ class CategoryController extends Controller
     }
     */
 
-    // getIndex PREGUNTAR SI ES CON BLADE O JS, FILTRADO
+    // getIndex
     public function getIndex(Request $request)
-{
-    $categories = Category::all();
-    $categoryId = $request->query('category');
+    {
+        $categories = Category::all();
+        $categoryId = $request->query('category');
 
-    if ($categoryId) {        
-        $posts = Post::where('id_category', $categoryId)->get();
-    } else {
-        $posts = Post::all();
+        if ($categoryId) {
+            $posts = Post::where('id_category', $categoryId)->get();
+        } else {
+            $posts = Post::all();
+        }
+
+        return view('category.index', compact('posts', 'categories'));
     }
-
-    return view('category.index', compact('posts', 'categories'));
-}
 
     // Mostrar el post (getShow)
     public function getShow($id)
     {
         $post = Post::findOrFail($id);
+        $category = Category::findOrFail($post->id_category);
+        $isOwner = Auth::id() === $post->id_user;
 
-        return view('category.show', compact('post'));
+        return view('category.show', compact('post', 'isOwner'), compact('category'));
     }
 
     // getCreate
@@ -79,6 +82,11 @@ class CategoryController extends Controller
         $post = Post::findOrFail($id);
         $categories = Category::all(); // Pasa las categorías
 
+        //  Si el usuario no es el dueño, abortar con error 403 (Prohibido)
+        if (auth()->id() !== $post->id_user) {
+            abort(403, 'No tenés permiso para editar este post.');
+        }
+
         return view('category.edit', compact('post', 'categories'));
     }
 
@@ -86,6 +94,10 @@ class CategoryController extends Controller
     public function update(Request $request, $id)
     {
         $post = Post::findOrFail($id);
+
+        if (auth()->id() !== $post->id_user) {
+            abort(403, 'No podés actualizar este post.');
+        }
 
         $post->title = $request->title;
         $post->poster = $request->poster;
--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -5,33 +5,36 @@
 @section('title', 'Detalle Blog')
 
 @section('content')
-    <div>        
+    <div>
         @if ($post)
             <div class="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 h-fit">
-                
-                <div class="flex flex-col px-2 rounded-2xl gap-2 size-auto">                    
-                    
+
+                <div class="flex flex-col px-2 rounded-2xl gap-2 size-auto">
+
                     <div class="text-5xl font-normal">{{ $post->title }}</div>
-                    
+
                     <div class="mt-3 flex justify-center items-center">
-                        <img class="w-[90%] h-screen rounded mb-1" src="{{ $post->poster }}"
-                            alt="{{ $post->title }}">
+                        <img class="w-[90%] h-screen rounded mb-1" src="{{ $post->poster }}" alt="{{ $post->title }}">
                     </div>
 
                     <div class="font-light first-line:text-3xl">
-                        {{ $post->content }} 
+                        {{ $post->content }}
                         <br>
                         <div class="pl-1">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, cum porro. Earum sit, molestias doloribus ea quas voluptas debitis consectetur in! Temporibus minima obcaecati odit numquam impedit necessitatibus doloremque laboriosam?
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, cum porro. Earum sit, molestias
+                            doloribus ea quas voluptas debitis consectetur in! Temporibus minima obcaecati odit numquam
+                            impedit necessitatibus doloremque laboriosam?
                         </div>
                     </div>
 
                 </div>
 
                 @auth
-                    <a href="{{ route('category.edit', $post->id) }}" class="w-screen flex justify-center">
-                        <button class="mb-4 mt-4 bg-gray-300 px-4 py-2 rounded hover:bg-gray-400 w-32">Editar</button>
-                    </a>
+                    @if ($isOwner)
+                        <a href="{{ route('category.edit', $post->id) }}" class="w-screen flex justify-center">
+                            <button class="mb-4 mt-4 px-4 py-2 rounded w-32 bg-gray-100 hover:bg-gray-200 ">Editar</button>
+                        </a>
+                    @endif
                 @endauth
             </div>
         @else
